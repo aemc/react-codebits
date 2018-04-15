05023a8cad88f8f3d5b1497302e80cc5c8598043
aemc <aemlcruz@gmail.com>
2018-04-15 12:54:53 -0400

    Add searchbar + update dependencies

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+// class-based component
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input />
+        return (
+            <div>
+                <input
+                    value = {this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })}/>
+            </div>
+        )
     }
 }
 
 export default SearchBar;
+
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCqMOiqcsoA-TFKSWwEWbxBQnTaGt2-m73';
 
-// Create a new component
-// This component should produce some HTML
-
+// function-based component (no states)
 const App = () => {
     return (
         <div>
@@ -15,8 +13,5 @@ const App = () => {
         </div>
     )
 };
-
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
 
 ReactDOM.render(<App />, document.querySelector('.container'));
