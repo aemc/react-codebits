2f6a86bd1d3ff6bedb9ebdffc2c6f8a20947e464
aemc <aemlcruz@gmail.com>
2018-04-17 14:11:44 -0400

    Commit latest changes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import rootReducers from './reducers';
 
 const store = createStore(rootReducers);
 console.log(store.getState());
-store.subscribe(() => console.log('store',store.getState()));
+store.subscribe(() => console.log('store', store.getState()));
 
 ReactDOM.render(
     <Provider store={store}>
